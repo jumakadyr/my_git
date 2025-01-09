d74d221624726a79f5d2a8be33d3bc3562b9a34a
Juma <jums92547@gmail.com>
2025-01-09 16:23:30 +0600

    .

--- home_work.py
+++ home_work.py
@@ -2,3 +2,11 @@ def summarize(*a):
     return sum(a)
 
 print(summarize(1, 2, 3, 4, 5))
+
+# 2
+
+def describe_person(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key.capitalize()}: {value}")
+
+describe_person(name="Жума", age=18, city="Бишкек")
