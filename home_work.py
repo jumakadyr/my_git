55defc7a2d6be2e66d7b443e619c5e1636341716
Juma <jums92547@gmail.com>
2025-01-09 16:24:11 +0600

    .

--- home_work.py
+++ home_work.py
@@ -10,3 +10,15 @@ def describe_person(**kwargs):
         print(f"{key.capitalize()}: {value}")
 
 describe_person(name="Жума", age=18, city="Бишкек")
+
+# 3
+def multiply_all(*args):
+    result = 1
+    for num in args:
+        result *= num
+    return result
+
+print(multiply_all(1, 2, 3, 4))  
+print(multiply_all(5, 6))        
+print(multiply_all())           
+
